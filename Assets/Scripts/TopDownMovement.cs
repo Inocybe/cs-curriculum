8b847e84e1ccf0cb5459d8b5046cc3d917a1702b
Inocybe <levmitchell@icloud.com>
2023-09-28 12:01:35 -0700

    be careful of movmenet inside the start
    it not owkring had to go

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -9,19 +9,31 @@ public class TopDownMovement : MonoBehaviour
     public float walkingSpeed;
     private float xDirection;
     private float yDriection;
+    private float originalSpeed;
 
+    void Start()
+    {
+        originalSpeed = walkingSpeed;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
     
     private void Move()
     {
         xDirection = Input.GetAxis("Horizontal");
         yDriection = Input.GetAxis("Vertical");
+
+        if (xDirection + yDriection != 0)
+        {
+            walkingSpeed /= 2;
+        }
         
         transform.position += new Vector3(xDirection * walkingSpeed * Time.deltaTime, yDriection * walkingSpeed * Time.deltaTime, 0);
+
+        walkingSpeed = originalSpeed;
     }
 
 }
