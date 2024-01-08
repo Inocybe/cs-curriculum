e349bce6ada0c0c577241704aeb4947a2b1b6d5b
Inocybe <levmitchell@icloud.com>
2024-01-08 11:21:43 -0800

    sda

--- Assets/Scripts/PlayerShootingScript.cs
+++ Assets/Scripts/PlayerShootingScript.cs
@@ -1,22 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerShootingScript : MonoBehaviour
 {
-    private SpriteRenderer sprite;
+    [SerializeField] public GameObject fireball;
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        sprite = GetComponent<SpriteRenderer>();
-    }
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
@@ -25,8 +22,10 @@ public class PlayerShootingScript : MonoBehaviour
 
     void Shoot()
     {
-        
-        //if (sprite.flipX == )
+        Vector2 mousePoint = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        Vector2 shootDirection = new Vector2(mousePoint.x - transform.position.x, mousePoint.y - transform.position.y).normalized * speed;
+        GameObject shot = Instantiate(fireball, transform.position, Quaternion.identity);
+        shot.GetComponent<Rigidbody2D>().velocity = shootDirection;
     }
     
     
